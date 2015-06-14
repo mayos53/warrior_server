5d78f99bbffed5c9049a4eb31e706a4c98881137
Yossi <yossi.assayag@gmail.com>
2015-06-14 13:03:41 +0300

    add require 'net/http' in environment.rb
    	modified:   config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+require 'net/http'
+
+
 # Initialize the Rails application.
 Warrior::Application.initialize!
+
